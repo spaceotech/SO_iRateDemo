319339d48b8c15036326872d942d23db38887ca8
Hitendra Mac <hitendra.spaceo@gmail.com>
2017-05-18 17:52:17 +0530

    -  Initial commute + working Example

--- iRateDemo/AppDelegate.swift
+++ iRateDemo/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        iRate.sharedInstance().daysUntilPrompt = 5;
+        iRate.sharedInstance().usesUntilPrompt = 20;
+        iRate.sharedInstance().appStoreID = 981778637;
         return true
     }
 
